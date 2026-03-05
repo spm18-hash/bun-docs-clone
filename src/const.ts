5166873033f2eb1d3ad48fe1f65bc73b618899a0
Smit Mistry <smitmistry19@gmail.com>
2026-03-05 11:48:57 +0530

    feat: add support for Cloudflare Pages preview deployments

--- src/const.ts
+++ src/const.ts
@@ -1,8 +1,13 @@
 export const PAGE_TITLE_ID = "_top";
 
+export const IS_PREVIEW =
+  process.env.CF_PAGES_BRANCH !== "main" && process.env.CF_PAGE === "1";
+
 export const SITE_URL =
-  process.env.NODE_ENV === "production"
-    ? "https://bun-docs-clone.pages.dev"
+  process.env.CF_PAGES === "1"
+    ? process.env.CF_PAGES_BRANCH === "main"
+      ? "https://bun-docs-clone.pages.dev" // Production URL
+      : process.env.CF_PAGES_URL // Unique Preview URL
     : "http://localhost:4321";
 
 export const SITE_TITLE = "Bun";
