a7690b54678d59b78337dbdf7261ea3c0c2804b5
Smit Mistry <smitmistry19@gmail.com>
2026-03-05 11:30:25 +0530

    Create llms.txt route

---
+++ src/pages/llms.txt.ts
@@ -0,0 +1,21 @@
+import type { APIRoute } from "astro";
+import { SITE_TITLE } from "@/const";
+import { getCollection } from "astro:content";
+import { composeMarkdown } from "./[...slug].md";
+import { site } from "astro:config/client";
+
+export const GET = (async () => {
+  const docs = await getCollection("docs");
+
+  const docLinks = docs
+    .map((doc) => `- [${doc.data.title}](${site}/raw/${doc.id}.md)`)
+    .join("\n");
+
+  const header = `# ${SITE_TITLE}\n\n## Docs\n\n`;
+
+  const final = composeMarkdown(header, docLinks);
+
+  return new Response(final, {
+    headers: { "Content-Type": "text/markdown; charset=utf-8" },
+  });
+}) satisfies APIRoute;
